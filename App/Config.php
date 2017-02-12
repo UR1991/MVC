394a99703d0195632137c9f94c6d271d68ede275
UR1991 <ultimaratio1991@gmail.com>
2017-02-13 01:22:58 +0300

    modified .htaccess

--- App/Config.php
+++ App/Config.php
@@ -1,9 +1,3 @@
 <?php
 //This is initialization file
-
-define ('DIRSEP', DIRECTORY_SEPARATOR);
-
-//route to main catalog
-$sitePath = realpath(dirname(__FILE__) . DIRSEP);
-define ('SITE_PATH', $sitePath);
  ?>
